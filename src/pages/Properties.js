66a221761149736dc2886ea865c60a47f490df66
Azeez <abdulazeezojo24@gmail.com>
2022-03-11 10:15:53 +0100

    main

--- src/pages/Properties.js
+++ src/pages/Properties.js
@@ -1,7 +1,38 @@
 import React from "react";
+import Navbar from "../components/Navbar/Navbar";
+import Card from "../components/Card/Card";
+import Footer from "../layout/Footer/Footer";
 
 const Properties = () => {
-  return <div>prop</div>;
+  return (
+    <div>
+      <Navbar />
+      <div
+        className="w-full bg-center bg-cover h-[32rem]"
+        style={{
+          backgroundImage:
+            "url(https://images.unsplash.com/photo-1504384308090-c894fdcc538d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80)",
+        }}
+      >
+        <div class="flex items-center justify-center w-full h-full bg-gray-900 bg-opacity-50">
+          <div class="text-center">
+            <h1 class="text-2xl font-semibold text-white uppercase lg:text-3xl">
+              Choose Your Desired Property{" "}
+            </h1>
+          </div>
+        </div>
+      </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <Card />
+      <br />
+      <br />
+      <br />
+      <Footer />
+    </div>
+  );
 };
 
 export default Properties;
