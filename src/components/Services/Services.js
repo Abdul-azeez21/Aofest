02e0888ca87272b21160318b7426064efd6dd584
Azeez <abdulazeezojo24@gmail.com>
2022-03-17 20:16:49 +0100

    latest

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -5,37 +5,30 @@ const Services = () => {
     <div>
       <section class="bg-white dark:bg-gray-900">
         <div class="container px-6 py-10 mx-auto">
-          <h1 class="text-3xl font-semibold text-gray-800 capitalize lg:text-4xl dark:text-white">
-            explore our <br /> awesome{" "}
-            <span class="underline decoration-blue-500">Components</span>
+          <h1 class="text-4xl font-semibold text-gray-800 capitalize lg:text-4xl dark:text-white">
+            Why Aofest?{" "}
           </h1>
 
-          <p class="mt-4 text-gray-500 xl:mt-6 dark:text-gray-300">
+          {/* <p class="mt-4 text-gray-500 xl:mt-6 dark:text-gray-300">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
             quam voluptatibus
-          </p>
+          </p> */}
 
           <div class="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 md:grid-cols-2 xl:grid-cols-3">
             <div class="p-8 space-y-3 border-2 border-blue-400 dark:border-blue-300 rounded-xl">
               <span class="inline-block text-blue-500 dark:text-blue-400">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  class="w-8 h-8"
+                  class="h-6 w-6"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
+                  stroke-width="2"
                 >
                   <path
                     stroke-linecap="round"
                     stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z"
-                  />
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z"
+                    d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
                   />
                 </svg>
               </span>
@@ -49,42 +42,22 @@ const Services = () => {
                 Provident ab nulla quod dignissimos vel non corrupti doloribus
                 voluptatum eveniet
               </p>
-
-              <a
-                href="#"
-                class="inline-flex p-2 text-blue-500 capitalize transition-colors duration-200 transform bg-blue-100 rounded-full dark:bg-blue-500 dark:text-white hover:underline hover:text-blue-600 dark:hover:text-blue-500"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="w-6 h-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                  />
-                </svg>
-              </a>
             </div>
 
             <div class="p-8 space-y-3 border-2 border-blue-400 dark:border-blue-300 rounded-xl">
               <span class="inline-block text-blue-500 dark:text-blue-400">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  class="w-8 h-8"
+                  class="h-6 w-6"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
+                  stroke-width="2"
                 >
                   <path
                     stroke-linecap="round"
                     stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M11 4a2 2 0 114 0v1a1 1 0 001 1h3a1 1 0 011 1v3a1 1 0 01-1 1h-1a2 2 0 100 4h1a1 1 0 011 1v3a1 1 0 01-1 1h-3a1 1 0 01-1-1v-1a2 2 0 10-4 0v1a1 1 0 01-1 1H7a1 1 0 01-1-1v-3a1 1 0 00-1-1H4a2 2 0 110-4h1a1 1 0 001-1V7a1 1 0 011-1h3a1 1 0 001-1V4z"
+                    d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"
                   />
                 </svg>
               </span>
@@ -98,42 +71,22 @@ const Services = () => {
                 Provident ab nulla quod dignissimos vel non corrupti doloribus
                 voluptatum eveniet
               </p>
-
-              <a
-                href="#"
-                class="inline-flex p-2 text-blue-500 capitalize transition-colors duration-200 transform bg-blue-100 rounded-full dark:bg-blue-500 dark:text-white hover:underline hover:text-blue-600 dark:hover:text-blue-500"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="w-6 h-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                  />
-                </svg>
-              </a>
             </div>
 
             <div class="p-8 space-y-3 border-2 border-blue-400 dark:border-blue-300 rounded-xl">
               <span class="inline-block text-blue-500 dark:text-blue-400">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  class="w-8 h-8"
+                  class="h-6 w-6"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
+                  stroke-width="2"
                 >
                   <path
                     stroke-linecap="round"
                     stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"
+                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
                   />
                 </svg>
               </span>
@@ -147,26 +100,6 @@ const Services = () => {
                 Provident ab nulla quod dignissimos vel non corrupti doloribus
                 voluptatum eveniet
               </p>
-
-              <a
-                href="#"
-                class="inline-flex p-2 text-blue-500 capitalize transition-colors duration-200 transform bg-blue-100 rounded-full dark:bg-blue-500 dark:text-white hover:underline hover:text-blue-600 dark:hover:text-blue-500"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="w-6 h-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                  />
-                </svg>
-              </a>
             </div>
           </div>
         </div>
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -37,12 +37,11 @@ const Homepage = () => {
       <br />
       <br />
       <br />
-      <div class="lg:flex">
+      {/* <div class="lg:flex">
         <div class="flex items-center justify-center w-full px-6 py-8 lg:h-[32rem] lg:w-1/2">
           <div class="max-w-xl">
             <h2 class="text-2xl font-semibold text-gray-800 dark:text-white lg:text-3xl">
               Build Your New{" "}
-              <span class="text-blue-600 dark:text-blue-400">Idea</span>
             </h2>
 
             <p class="mt-2 text-sm text-gray-500 dark:text-gray-400 lg:text-base">
@@ -50,24 +49,8 @@ const Homepage = () => {
               Blanditiis commodi cum cupiditate ducimus, fugit harum id
               necessitatibus odio quam quasi, quibusdam rem tempora voluptates.
             </p>
-
-            <div class="flex flex-col mt-6 space-y-3 lg:space-y-0 lg:flex-row">
-              <a
-                href="#"
-                class="block px-3 py-2 text-sm font-semibold text-center text-white transition-colors duration-200 transform bg-gray-900 rounded-md hover:bg-gray-700"
-              >
-                Get Started
-              </a>
-              <a
-                href="#"
-                class="block px-3 py-2 text-sm font-semibold text-center text-gray-700 transition-colors duration-200 transform bg-gray-200 rounded-md lg:mx-4 hover:bg-gray-300"
-              >
-                Learn More
-              </a>
-            </div>
           </div>
         </div>
-
         <div class="w-full h-64 lg:w-1/2 lg:h-auto">
           <div
             class="w-full h-full bg-cover"
@@ -79,9 +62,7 @@ const Homepage = () => {
             <div class="w-full h-full bg-black opacity-25"></div>
           </div>
         </div>
-      </div>
-      <br />
-      <br />
+      </div> */}
       <br />
       <br />
       {/* <Carousel /> */}
@@ -91,239 +72,6 @@ const Homepage = () => {
       <Stats />
       <br />
       <br />
-      <br />
-      <br />
-      <ul
-        class="
-  nav nav-tabs nav-justified
-  flex flex-col
-  md:flex-row
-  flex-wrap
-  list-none
-  border-b-0
-  pl-0
-  mb-4
-"
-        id="tabs-tabJustify"
-        role="tablist"
-      >
-        <li class="nav-item flex-grow text-center" role="presentation">
-          <a
-            href="#tabs-homeJustify"
-            class="
-      nav-link
-      w-full
-      block
-      font-medium
-      text-xs
-      leading-tight
-      uppercase
-      border-x-0 border-t-0 border-b-2 border-transparent
-      px-6
-      py-3
-      my-2
-      hover:border-transparent hover:bg-gray-100
-      focus:border-transparent
-      active
-    "
-            id="tabs-home-tabJustify"
-            data-bs-toggle="pill"
-            data-bs-target="#tabs-homeJustify"
-            role="tab"
-            aria-controls="tabs-homeJustify"
-            aria-selected="true"
-          >
-            Home
-          </a>
-        </li>
-        <li class="nav-item flex-grow text-center" role="presentation">
-          <a
-            href="#tabs-profileJustify"
-            class="
-      nav-link
-      w-full
-      bloc
-      font-medium
-      text-xs
-      leading-tight
-      uppercase
-      border-x-0 border-t-0 border-b-2 border-transparent
-      px-6
-      py-3
-      my-2
-      hover:border-transparent hover:bg-gray-100
-      focus:border-transparent
-    "
-            id="tabs-profile-tabJustify"
-            data-bs-toggle="pill"
-            data-bs-target="#tabs-profileJustify"
-            role="tab"
-            aria-controls="tabs-profileJustify"
-            aria-selected="false"
-          >
-            Very very very very long link
-          </a>
-        </li>
-        <li class="nav-item flex-grow text-center" role="presentation">
-          <a
-            href="#tabs-messagesJustify"
-            class="
-      nav-link
-      w-full
-      block
-      font-medium
-      text-xs
-      leading-tight
-      uppercase
-      border-x-0 border-t-0 border-b-2 border-transparent
-      px-6
-      py-3
-      my-2
-      hover:border-transparent hover:bg-gray-100
-      focus:border-transparent
-    "
-            id="tabs-messages-tabJustify"
-            data-bs-toggle="pill"
-            data-bs-target="#tabs-messagesJustify"
-            role="tab"
-            aria-controls="tabs-messagesJustify"
-            aria-selected="false"
-          >
-            Messages
-          </a>
-        </li>
-      </ul>
-      <div class="tab-content" id="tabs-tabContentJustify">
-        <div
-          class="tab-pane fade show active"
-          id="tabs-homeJustify"
-          role="tabpanel"
-          aria-labelledby="tabs-home-tabJustify"
-        >
-          <section class="bg-gray-100 dark:bg-gray-900 lg:py-12 lg:flex lg:justify-center">
-            <div class="bg-white dark:bg-gray-800 lg:mx-8 lg:flex lg:max-w-5xl lg:shadow-lg lg:rounded-lg">
-              <div class="lg:w-1/2">
-                <div
-                  class="h-64 bg-cover lg:rounded-lg lg:h-full"
-                  style={{
-                    backgroundImage:
-                      "url('https://images.unsplash.com/photo-1593642532400-2682810df593?ixlib=rb-1.2.1&ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=750&q=80')",
-                  }}
-                ></div>
-              </div>
-
-              <div class="max-w-xl px-6 py-12 lg:max-w-5xl lg:w-1/2">
-                <h2 class="text-2xl font-bold text-gray-800 dark:text-white md:text-3xl">
-                  Build Your New{" "}
-                  <span class="text-blue-600 dark:text-blue-400">Idea</span>
-                </h2>
-                <p class="mt-4 text-gray-600 dark:text-gray-400">
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                  Quidem modi reprehenderit vitae exercitationem aliquid dolores
-                  ullam temporibus enim expedita aperiam mollitia iure
-                  consectetur dicta tenetur, porro consequuntur saepe
-                  accusantium consequatur.
-                </p>
-
-                <div class="mt-8">
-                  <a
-                    href="#"
-                    class="px-5 py-2 font-semibold text-gray-100 transition-colors duration-200 transform bg-gray-900 rounded-md hover:bg-gray-700"
-                  >
-                    Start Now
-                  </a>
-                </div>
-              </div>
-            </div>
-          </section>
-        </div>
-        <div
-          class="tab-pane fade"
-          id="tabs-profileJustify"
-          role="tabpanel"
-          aria-labelledby="tabs-profile-tabJustify"
-        >
-          <section class="bg-gray-100 dark:bg-gray-900 lg:py-12 lg:flex lg:justify-center">
-            <div class="bg-white dark:bg-gray-800 lg:mx-8 lg:flex lg:max-w-5xl lg:shadow-lg lg:rounded-lg">
-              <div class="lg:w-1/2">
-                <div
-                  class="h-64 bg-cover lg:rounded-lg lg:h-full"
-                  style={{
-                    backgroundImage:
-                      "url('https://images.unsplash.com/photo-1593642532400-2682810df593?ixlib=rb-1.2.1&ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=750&q=80')",
-                  }}
-                ></div>
-              </div>
-
-              <div class="max-w-xl px-6 py-12 lg:max-w-5xl lg:w-1/2">
-                <h2 class="text-2xl font-bold text-gray-800 dark:text-white md:text-3xl">
-                  Build Your New{" "}
-                  <span class="text-blue-600 dark:text-blue-400">Idea</span>
-                </h2>
-                <p class="mt-4 text-gray-600 dark:text-gray-400">
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                  Quidem modi reprehenderit vitae exercitationem aliquid dolores
-                  ullam temporibus enim expedita aperiam mollitia iure
-                  consectetur dicta tenetur, porro consequuntur saepe
-                  accusantium consequatur.
-                </p>
-
-                <div class="mt-8">
-                  <a
-                    href="#"
-                    class="px-5 py-2 font-semibold text-gray-100 transition-colors duration-200 transform bg-gray-900 rounded-md hover:bg-gray-700"
-                  >
-                    Start Now
-                  </a>
-                </div>
-              </div>
-            </div>
-          </section>
-        </div>
-        <div
-          class="tab-pane fade"
-          id="tabs-messagesJustify"
-          role="tabpanel"
-          aria-labelledby="tabs-profile-tabJustify"
-        >
-          <section class="bg-gray-100 dark:bg-gray-900 lg:py-12 lg:flex lg:justify-center">
-            <div class="bg-white dark:bg-gray-800 lg:mx-8 lg:flex lg:max-w-5xl lg:shadow-lg lg:rounded-lg">
-              <div class="lg:w-1/2">
-                <div
-                  class="h-64 bg-cover lg:rounded-lg lg:h-full"
-                  style={{
-                    backgroundImage:
-                      "url('https://images.unsplash.com/photo-1593642532400-2682810df593?ixlib=rb-1.2.1&ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=750&q=80')",
-                  }}
-                ></div>
-              </div>
-
-              <div class="max-w-xl px-6 py-12 lg:max-w-5xl lg:w-1/2">
-                <h2 class="text-2xl font-bold text-gray-800 dark:text-white md:text-3xl">
-                  Build Your New{" "}
-                  <span class="text-blue-600 dark:text-blue-400">Idea</span>
-                </h2>
-                <p class="mt-4 text-gray-600 dark:text-gray-400">
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                  Quidem modi reprehenderit vitae exercitationem aliquid dolores
-                  ullam temporibus enim expedita aperiam mollitia iure
-                  consectetur dicta tenetur, porro consequuntur saepe
-                  accusantium consequatur.
-                </p>
-
-                <div class="mt-8">
-                  <a
-                    href="#"
-                    class="px-5 py-2 font-semibold text-gray-100 transition-colors duration-200 transform bg-gray-900 rounded-md hover:bg-gray-700"
-                  >
-                    Start Now
-                  </a>
-                </div>
-              </div>
-            </div>
-          </section>
-        </div>
-      </div>
       <Agents />
       <br />
       <br />
