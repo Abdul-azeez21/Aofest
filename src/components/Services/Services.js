5d7aa78827c9d92c8a364df6e6f6c844dea438e5
Azeez <abdulazeezojo24@gmail.com>
2022-07-19 12:13:13 +0100

    transform from static to dynamic begins

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,134 +1,103 @@
 import React from "react";
 
+//motion
+import { motion } from "framer-motion";
+
+//icons
+import { FiTruck, FiShoppingCart, FiHome, FiUser } from "react-icons/fi";
+import { HiOutlineChartBar } from "react-icons/hi";
+
 const Services = () => {
   return (
     <div>
-      <section class="bg-white dark:bg-gray-900">
-        <div class="container px-6 py-10 mx-auto">
-          <h1 class="text-4xl font-semibold text-gray-800 capitalize lg:text-4xl dark:text-white">
-            What we do{" "}
+      <section className="bg-white dark:bg-gray-900">
+        <div className="container px-6 py-10 mx-auto">
+          <h1 className="text-xl font-semibold text-gray-800 lg:text-2xl dark:text-white">
+            What is{" "}
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-sky-900">
+              AOFEST
+            </span>{" "}
+            all about?
           </h1>
 
-          {/* <p class="mt-4 text-gray-500 xl:mt-6 dark:text-gray-300">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
-            quam voluptatibus
-          </p> */}
-
-          <div class="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 md:grid-cols-2 xl:grid-cols-5">
-            <div class="p-8 space-y-3 border-2 border-blue-400 dark:border-blue-300 rounded-xl">
-              <span class="inline-block text-blue-500 dark:text-blue-400">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  stroke-width="2"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-                  />
-                </svg>
+          <motion.div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 md:grid-cols-2 xl:grid-cols-5">
+            <motion.div
+              className="p-8 space-y-3 border-2 border-blue-400 dark:border-blue-300 rounded-xl"
+              initial={{ opacity: 0, translateX: -50 }}
+              whileInView={{ opacity: 1, translateX: 0 }}
+              transition={{ duration: 1, delay: 1 }}
+              viewport={{ once: true }}
+            >
+              <span className="inline-block text-blue-500 dark:text-blue-400">
+                <FiHome className="h-6 w-6 animate-bounce" strokeWidth="2" />
               </span>
-
-              <h1 class="text-2xl font-semibold text-gray-700 capitalize dark:text-white">
+              <h1 className="text-xl font-semibold text-gray-700 capitalize dark:text-white">
                 Real Estate & Property Management
               </h1>
-            </div>
-
-            <div class="p-8 space-y-3 border-2 border-blue-400 dark:border-blue-300 rounded-xl">
-              <span class="inline-block text-blue-500 dark:text-blue-400">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  stroke-width="2"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                  />
-                </svg>
+            </motion.div>
+            <motion.div
+              className="p-8 space-y-3 border-2 border-blue-400 dark:border-blue-300 rounded-xl"
+              initial={{ opacity: 0, translateX: -50 }}
+              whileInView={{ opacity: 1, translateX: 0 }}
+              transition={{ duration: 1, delay: 1 }}
+              viewport={{ once: true }}
+            >
+              <span className="inline-block text-blue-500 dark:text-blue-400">
+                <FiShoppingCart
+                  className="h-6 w-6 animate-bounce"
+                  strokeWidth="2"
+                />
               </span>
-
-              <h1 class="text-2xl font-semibold text-gray-700 capitalize dark:text-white">
+              <h1 className="text-xl font-semibold text-gray-700 capitalize dark:text-white">
                 Sales & Purchase of Landed Property
               </h1>
-            </div>
-
-            <div class="p-8 space-y-3 border-2 border-blue-400 dark:border-blue-300 rounded-xl">
+            </motion.div>
+            <motion.div
+              className="p-8 space-y-3 border-2 border-blue-400 dark:border-blue-300 rounded-xl"
+              initial={{ opacity: 0, translateX: -50 }}
+              whileInView={{ opacity: 1, translateX: 0 }}
+              transition={{ duration: 1, delay: 1 }}
+              viewport={{ once: true }}
+            >
               <span class="inline-block text-blue-500 dark:text-blue-400">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  stroke-width="2"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
-                  />
-                </svg>
+                <HiOutlineChartBar
+                  className="h-6 w-6 animate-bounce"
+                  strokeWidth="2"
+                />
               </span>
-
-              <h1 class="text-2xl font-semibold text-gray-700 capitalize dark:text-white">
+              <h1 className="text-xl font-semibold text-gray-700 capitalize dark:text-white">
                 Development Services
               </h1>
-            </div>
-            <div class="p-8 space-y-3 border-2 border-blue-400 dark:border-blue-300 rounded-xl">
-              <span class="inline-block text-blue-500 dark:text-blue-400">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  stroke-width="2"
-                >
-                  <path d="M9 17a2 2 0 11-4 0 2 2 0 014 0zM19 17a2 2 0 11-4 0 2 2 0 014 0z" />
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M13 16V6a1 1 0 00-1-1H4a1 1 0 00-1 1v10a1 1 0 001 1h1m8-1a1 1 0 01-1 1H9m4-1V8a1 1 0 011-1h2.586a1 1 0 01.707.293l3.414 3.414a1 1 0 01.293.707V16a1 1 0 01-1 1h-1m-6-1a1 1 0 001 1h1M5 17a2 2 0 104 0m-4 0a2 2 0 114 0m6 0a2 2 0 104 0m-4 0a2 2 0 114 0"
-                  />
-                </svg>
+            </motion.div>
+            <motion.div
+              className="p-8 space-y-3 border-2 border-blue-400 dark:border-blue-300 rounded-xl"
+              initial={{ opacity: 0, translateX: -50 }}
+              whileInView={{ opacity: 1, translateX: 0 }}
+              transition={{ duration: 1, delay: 1 }}
+              viewport={{ once: true }}
+            >
+              <span className="inline-block text-blue-500 dark:text-blue-400">
+                <FiTruck className="h-6 w-6 animate-bounce" strokeWidth="2" />
               </span>
-
-              <h1 class="text-2xl font-semibold text-gray-700 capitalize dark:text-white">
+              <h1 className="text-xl font-semibold text-gray-700 capitalize dark:text-white">
                 Plant Hiring
               </h1>
-            </div>
-            <div class="p-8 space-y-3 border-2 border-blue-400 dark:border-blue-300 rounded-xl">
-              <span class="inline-block text-blue-500 dark:text-blue-400">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  stroke-width="2"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-                  />
-                </svg>
-              </span>
-
-              <h1 class="text-2xl font-semibold text-gray-700 capitalize dark:text-white">
+            </motion.div>
+            <motion.div
+              className="p-8 space-y-3 border-2 border-blue-400 dark:border-blue-300 rounded-xl"
+              initial={{ opacity: 0, translateX: -50 }}
+              whileInView={{ opacity: 1, translateX: 0 }}
+              transition={{ duration: 1, delay: 1 }}
+              viewport={{ once: true }}
+            >
+              <motion.span className="inline-block text-blue-500 dark:text-blue-400">
+                <FiUser className="h-6 w-6 animate-bounce" strokeWidth="2" />
+              </motion.span>
+              <h1 className="text-xl font-semibold text-gray-700 capitalize dark:text-white">
                 General Contractor
               </h1>
-            </div>
-          </div>
+            </motion.div>
+          </motion.div>
         </div>
       </section>
     </div>
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,72 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar/Navbar";
 import Services from "../components/Services/Services";
 import Card from "../components/Card/Card.js";
-import Carousel from "../components/Carousel/Carousel";
 import Stats from "../components/Stats/Stats";
 import Agents from "../components/Agents/Agents";
 import Footer from "../layout/Footer/Footer";
 import "../css/homepage.css";
+
+//motion
+import { motion } from "framer-motion";
+
 const Homepage = () => {
   return (
     <div>
       <Navbar />
-      <div
-        className="w-full bg-center bg-cover h-[32rem]"
-        style={{
-          backgroundImage:
-            "url(https://images.unsplash.com/photo-1448630360428-65456885c650?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZXN0YXRlfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60)",
-        }}
-      >
-        <div class="flex items-center justify-center w-full h-full bg-gray-900 bg-opacity-50">
-          <div class="text-center">
-            <h1 class="text-2xl font-semibold text-white uppercase lg:text-3xl">
-              Buy & Build Affordable Residential Land{" "}
-            </h1>
+      <motion.div className="w-full bg-gradient-to-b from-blue-800 via-purple-400 to-white content-center justify-center items-center lg:flex h-[32rem] p-8">
+        <motion.div className="p-5">
+          <motion.div
+            className="border border-white p-2"
+            initial={{ opacity: 0, translateX: 50 }}
+            whileInView={{ opacity: 1, translateX: 0 }}
+            transition={{ duration: 1.5, delay: 0 }}
+            viewport={{ once: true }}
+          >
+            <motion.img
+              initial={{ opacity: 0, translateX: -70 }}
+              whileInView={{ opacity: 1, translateX: 0 }}
+              transition={{ duration: 1.5, delay: 0 }}
+              viewport={{ once: true }}
+              src="https://images.unsplash.com/photo-1448630360428-65456885c650?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZXN0YXRlfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+            ></motion.img>
+          </motion.div>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, translateX: 70 }}
+          whileInView={{ opacity: 1, translateX: 0 }}
+          transition={{ duration: 1.5, delay: 0 }}
+          viewport={{ once: true }}
+        >
+          <div className="scroller">
+            <span className="text-5xl text-gray-900">
+              Build
+              <br />
+              Buy
+              <br />
+              Manage
+              <br />
+              Sell
+              <br />
+              Renovate
+            </span>
           </div>
-        </div>
-      </div>
+          <span className="lg:text-4xl text-3xl text-gray-900 p-1">
+            Residential properties
+          </span>
+        </motion.div>
+      </motion.div>
       <Services />
       <div>
-        <h2 className="haa text-4xl font-bold justify-center w-full">
+        <h2 className="haa text-3xl font-semibold justify-center w-full">
           Properties
         </h2>
       </div>
       <Card />
       <br />
-      <br />
-      <br />
-      <br />
-      {/* <div class="lg:flex">
-        <div class="flex items-center justify-center w-full px-6 py-8 lg:h-[32rem] lg:w-1/2">
-          <div class="max-w-xl">
-            <h2 class="text-2xl font-semibold text-gray-800 dark:text-white lg:text-3xl">
-              Build Your New{" "}
-            </h2>
+      <section className="scroll">
+        <div className="scroll-inner">
+          <h1 className="lg:text-7xl text-5xl items-center justify-center flex text-white str">
+            <span className="px-2 scroller-item">BUILD</span>
+            <span className="px-2 scroller-item">BUY</span>
+            <span className="px-2 scroller-item">MANAGE</span>
+            <span className="px-2 scroller-item">SELL</span>
+            <span className="px-2 scroller-item">RENOVATE</span>
+            {/* Exact copy */}
+            <span className="px-2 scroller-item">BUILD</span>
+            <span className="px-2 scroller-item">BUY</span>
+            <span className="px-2 scroller-item">MANAGE</span>
+            <span className="px-2 scroller-item">SELL</span>
+            <span className="px-2 scroller-item">RENOVATE</span>
+          </h1>
+        </div>
+      </section>
 
-            <p class="mt-2 text-sm text-gray-500 dark:text-gray-400 lg:text-base">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Blanditiis commodi cum cupiditate ducimus, fugit harum id
-              necessitatibus odio quam quasi, quibusdam rem tempora voluptates.
-            </p>
-          </div>
-        </div>
-        <div class="w-full h-64 lg:w-1/2 lg:h-auto">
-          <div
-            class="w-full h-full bg-cover"
-            style={{
-              backgroundImage:
-                "url(https://images.unsplash.com/photo-1508394522741-82ac9c15ba69?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=748&q=80)",
-            }}
-          >
-            <div class="w-full h-full bg-black opacity-25"></div>
-          </div>
-        </div>
-      </div> */}
       <br />
       <br />
-      {/* <Carousel /> */}
-      <h1 className="haa text-4xl font-bold justify-center w-full">
+      <h1 className="haa text-3xl font-semibold justify-center w-full">
         Our Stats
       </h1>
       <Stats />
