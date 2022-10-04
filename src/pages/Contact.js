bd38fbb4f3afe968814a97a96b51cba941fd481d
Azeez <abdulazeezojo24@gmail.com>
2022-10-04 13:02:12 +0100

    contact banner

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -61,7 +61,7 @@ const Contact = () => {
             "url(https://images.unsplash.com/photo-1423666639041-f56000c27a9a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y29udGFjdCUyMHVzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60)",
         }}
       >
-        <div class="flex items-center justify-center w-full h-full bg-blue-900/25  backdrop-blur-sm">
+        <div class="flex items-center justify-center w-full h-full bg-blue-900/50">
           <div class="text-center">
             <h1 class="text-3xl font-semibold text-white lg:text-5xl">
               Contact Us{" "}
